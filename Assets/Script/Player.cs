bf782564438ffa0325eb966f641e5f1a99f33601
0105 <23ci0122@jec.ac.jp>
2025-06-12 16:29:52 +0900

    audio

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -1,7 +1,8 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Player : MonoBehaviour {
+public class Player : MonoBehaviour
+{
     public GameObject bulletPrefab; // 弾のプレハブ
     public float bulletSpeed = 10f;
     public float lifetime = 3f;
@@ -21,22 +22,24 @@ public class Player : MonoBehaviour {
     public float maxBulletSpeed = 20f;
     public float maxHoldTime = 3f; // 最大チャージ時間
 
-    void Start() {
+    void Start()
+    {
         currentHP = maxHP;
         audioSource = GetComponent<AudioSource>();
     }
-    void Update() {
+    void Update()
+    {
 
         if (isHolding)
         {
             float holdTime = Time.time - mouseDownTime;
             float ratio = Mathf.Clamp01(holdTime / maxHoldTime);
             atkbarImage.rectTransform.sizeDelta = new Vector2(ratio * barImage.rectTransform.sizeDelta.x, barImage.rectTransform.sizeDelta.y); // 
-            if (ratio >=0.66f)
+            if (ratio >= 0.66f)
             {
                 atkbarImage.color = Color.red; //
             }
-        
+
         }
         else
         {
@@ -51,7 +54,8 @@ public class Player : MonoBehaviour {
             isHolding = true;
         }
         //if (Input.GetMouseButtonDown(0)) {
-        if (Input.GetMouseButtonUp(0)) {
+        if (Input.GetMouseButtonUp(0))
+        {
             // 押下時間を計算
             float holdTime = Time.time - mouseDownTime;
             // 最大チャージ時間でクランプ
@@ -68,30 +72,33 @@ public class Player : MonoBehaviour {
             // 弾を生成して発射
             GameObject bullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
             Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
-            if (rb != null) {
+            if (rb != null)
+            {
                 rb.linearVelocity = dir * bulletSpeed;
-                Destroy(bullet,lifetime); // lifetime後に弾を破棄
+                Destroy(bullet, lifetime); // lifetime後に弾を破棄
             }
             // ダメージ値をセット
             PlayerBullet playerBullet = bullet.GetComponent<PlayerBullet>();
             if (playerBullet != null)
             {
-                playerBullet.damage = holdTime*2;
+                playerBullet.damage = holdTime * 2;
             }
             isHolding = false;
         }
     }
 
-    public void TakeDamage(int damage) {
+    public void TakeDamage(int damage)
+    {
         currentHP -= damage;
         audioSource.PlayOneShot(damageSE);
 
         float ratio = Mathf.Clamp01((float)currentHP / maxHP);
         //Debug.Log("ratio: " + ratio,gameObject);
-        barImage.rectTransform.sizeDelta = new Vector2(ratio * barImage.rectTransform.sizeDelta.x,barImage.rectTransform.sizeDelta.y); // 
+        barImage.rectTransform.sizeDelta = new Vector2(ratio * barImage.rectTransform.sizeDelta.x, barImage.rectTransform.sizeDelta.y); // 
         //Debug.Log("barImage.rectTransform.sizeDelta.x: " + barImage.rectTransform.sizeDelta.x,gameObject);
 
-        if (currentHP <= 0) {
+        if (currentHP <= 0)
+        {
             gameManager.GameOver();
             Destroy(gameObject);
         }
